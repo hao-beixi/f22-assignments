90f933d0e5fc307cb75fd89091479da451529dc4
Marynel Vazquez <marynel.vazquez@yale.edu>
2022-09-12 22:01:02 -0400

    added dep to a2

--- assignment-2/install_python_deps.sh
+++ assignment-2/install_python_deps.sh
@@ -2,7 +2,7 @@
 pip3 install --user -U pip
 
 # install tensorflow and other python deps
-pip3 install -U --user tensorflow==2.6.0 tensorboard==2.7.0 scipy matplotlib scikit-learn gdown
+pip3 install -U --user tensorflow==2.6.0 tensorboard==2.7.0 scipy matplotlib scikit-learn gdown testresources
 
 # install urdf parser for imitation learning 
 pip3 install --user -U urdf-parser-py
